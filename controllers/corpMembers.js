d73a0312161ce1ae789f31ba1bf714c2dcc05949
wachukxs <nwachukwuossai@gmail.com>
2024-11-29 03:15:37 +0000

    removed stale code.

--- controllers/corpMembers.js
+++ controllers/corpMembers.js
@@ -16,11 +16,6 @@ router.post(
 );
 
 // great resource for express route regex https://www.kevinleary.net/regex-route-express/ & https://forbeslindesay.github.io/express-route-tester/
-let years = parseInt(new Date(Date.now()).getFullYear().toFixed().slice(2, 4));
-let yearrange = "(" + (years - 1).toString() + "|" + years.toString() + ")"; // (18|19)
-
-// let route = '/:state((AB|AD|AK|AN|BA|BY|BN|BO|CR|DT|EB|ED|EK|EN|FC|GM|IM|JG|KD|KN|KT|KB|KG|KW|LA|NS|NG|OG|OD|OS|OY|PL|RV|SO|TR|YB|ZM|ab|ad|ak|an|ba|by|bn|bo|cr|dt|eb|ed|ek|en|fc|gm|im|jg|kd|kn|kt|kb|kg|kw|la|ns|ng|og|od|os|oy|pl|rv|so|tr|yb|zm))/:year_batch((' + yearrange + '([abcACB])))/:lastfour(([0-9]{4}))'
-// let route = '/:state((AB|AD|AK|AN|BA|BY|BN|BO|CR|DT|EB|ED|EK|EN|FC|GM|IM|JG|KD|KN|KT|KB|KG|KW|LA|NS|NG|OG|OD|OS|OY|PL|RV|SO|TR|YB|ZM|ab|ad|ak|an|ba|by|bn|bo|cr|dt|eb|ed|ek|en|fc|gm|im|jg|kd|kn|kt|kb|kg|kw|la|ns|ng|og|od|os|oy|pl|rv|so|tr|yb|zm))/:year_batch((' + '([0-9][0-9])' + '([abcACB])))/:lastfour(([0-9]{4}))'
 router.get(
   "/unread-messages",
   auth.verifyJWT,
